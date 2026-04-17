f84fda0615347515c2e815feb945763f4126a126
Livia <liviarec.28@gmail.com>
2026-04-17 18:39:54 +0300

    putin din tot

--- EX2/include/TimeLineManager.hpp
+++ EX2/include/TimeLineManager.hpp
@@ -9,10 +9,74 @@ namespace tlm {
     };
     
     struct Event {
-
+        int id;
+        string descrition;
+        int year;
+        int impact;
     };
 
     class TimeLineManager {
+        private:
+        LinkedList<Event> list;
+
+        public:
+        void recordAncientEvent(Event e) {
+            list.addFirst(e);
+        }
+
+        void recorRecentEvent(Event e) {
+            list.addLast(e);
+        }
+
+        void insertEventBetween(int leftId, int rightId, Event e) {
+            Node<Event>* current = list.pfirst;
+
+            while (current != NULL) {
+                if(current->info.id == leftId){
+                    if(current->next != NULL && current->next->info.id == rightId) {
+                        Node<Event>* newEvent = new Node<Event>;
+                        newEvent->info = e;
+
+                        Node<Event>* rightId = current->next;
+
+                        newEvent->next = rightId;
+                        newEvent->prev = current;
+                        current->next = newEvent;
+                        rightId->prev = newEvent;
+
+
+                    }
+                }
+
+                current = current->next;
+            }
+            cout << "IDs are not next to eachother: " << leftId << " - " << rightId << endl;
+
+        }
+
+        void eraseCorruptedEvent(int id) {
+
+        }
+
+        void relocateEvent(int idToMove, int newNeighborID, bool before) {
+
+        }
+
+        void displayChronology() {
+            if(list.isEmpty()) {
+                cout << "Timeline is empty. \n";
+            }
+
+        }
+
+        void computeTotalImpact() {
+
+        }
+
+        void stabilizeTimeline(int threshold) {
+            
+        }
+
 
     };
 }
